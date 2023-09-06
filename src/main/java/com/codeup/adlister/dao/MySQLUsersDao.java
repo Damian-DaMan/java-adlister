a0e0cab59e8907bc171863190ab9c671226ea915
Damian Solis <damianrsolis@gmail.com>
2023-09-06 12:29:23 -0500

    fixed

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,13 +1,16 @@
 package com.codeup.adlister.dao;
 
+
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.utils.Config;
 import com.mysql.cj.jdbc.Driver;
 
-import javax.servlet.jsp.jstl.core.Config;
 import java.sql.*;
 
 public class MySQLUsersDao implements Users {
+
     private Connection connection = null;
+
     public MySQLUsersDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
@@ -19,19 +22,20 @@ public class MySQLUsersDao implements Users {
         } catch (SQLException e) {
             throw new RuntimeException("Error connecting to the database!", e);
         }
-
+    }
 
     @Override
     public User findByUsername(String username) {
         String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
         try {
-            PreparedStatement statement = connection.preparteStatment(query);
+            PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, username);
             ResultSet rs = statement.executeQuery();
             if(rs.next()) {
                 return new User(
                         rs.getLong("id"),
                         rs.getString("username"),
+//                        username,  this is possible, but maybe not a good idea
                         rs.getString("email"),
                         rs.getString("password")
                 );
@@ -42,27 +46,24 @@ public class MySQLUsersDao implements Users {
         return null;
     }
 
-
-
     @Override
     public Long insert(User user) {
-            String query = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
-            try {
-                PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-                statement.setString(1, user.getUsername());
-                statement.setString(2, user.getEmail());
-                statement.setString(3, user.getPassword());
+        String query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
 
-                statement.executeUpdate();
-                ResultSet rs = statement.getGeneratedKeys();
+            statement.executeUpdate();
 
-                if(rs.next()) {
-                    return rs.getLong(1);
-                }
+            ResultSet rs = statement.getGeneratedKeys();
 
-            } catch (SQLException e) {
-                throw new RuntimeException("Error adding user to db", e);
+            if(rs.next()) {
+                return rs.getLong(1);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error adding user to database", e);
         }
         return -1L;
     }
